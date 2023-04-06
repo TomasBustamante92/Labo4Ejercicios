45a34acb499e3bf083cb78601a4842333959c0d4
TomasBustamante92 <tom.sb92@gmail.com>
2023-04-06 13:11:36 -0300

    a

---
+++ ejercicio1/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  edadUno = '';
+  edadDos = '';
+  suma = '';
+  promedio = '';
+
+  calcular(){
+    let sumar = Number(this.edadUno) + Number(this.edadDos);
+    this.suma = String(sumar);
+    this.promedio = String(sumar / 2);
+  }
+}
